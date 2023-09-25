b6d45d076c4388d826a52afa1e4f154bd5fed321
Yilong Li <mondaycha@outlook.com>
2023-09-25 10:27:15 +0800

    feat: add axios api

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import Login from "./pages/Login";
 import DashboardLayout from "./pages/Dashboard/Admin/Layout";
 import { useAuth } from "./hooks/useAuth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -55,6 +56,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,
---
+++ src/services/types.ts
@@ -0,0 +1,6 @@
+// https://codevoweb.com/react-query-context-api-axios-interceptors-jwt-auth/
+export interface IUser {
+  name: string;
+  id: string;
+  token: string;
+}
