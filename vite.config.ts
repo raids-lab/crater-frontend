184be9c06bf88093bdc2f970ad4f99f3450bc856
Yilong Li <mondaycha@outlook.com>
2023-11-21 11:51:50 +0800

    chore: cdn for react and react-dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
-import path from "path";
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import inspect from "vite-plugin-inspect";
+import { cdn } from "vite-plugin-cdn2";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      react(),
+      cdn({
+        modules: [
+          { name: "react", relativeModule: "./umd/react.production.min.js" },
+          {
+            name: "react-dom",
+            relativeModule: "./umd/react-dom.production.min.js",
+            aliases: ["client"],
+          },
+        ],
+        apply: command,
+      }),
+      inspect(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  build: {
-    sourcemap: false,
-  },
+  };
 });
