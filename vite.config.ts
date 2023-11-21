25f70ca424f5ea08ddc77237c14df944c823544d
Yilong Li <mondaycha@outlook.com>
2023-11-21 15:48:10 +0800

    fix: vite hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -4,27 +4,24 @@ import react from "@vitejs/plugin-react-swc";
 import inspect from "vite-plugin-inspect";
 import { cdn } from "vite-plugin-cdn2";
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [
-      react(),
-      cdn({
-        modules: [
-          { name: "react", relativeModule: "./umd/react.production.min.js" },
-          {
-            name: "react-dom",
-            relativeModule: "./umd/react-dom.production.min.js",
-            aliases: ["client"],
-          },
-        ],
-        apply: command,
-      }),
-      inspect(),
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+export default defineConfig({
+  plugins: [
+    react(),
+    cdn({
+      modules: [
+        { name: "react", relativeModule: "./umd/react.production.min.js" },
+        {
+          name: "react-dom",
+          relativeModule: "./umd/react-dom.production.min.js",
+          aliases: ["client"],
+        },
+      ],
+    }),
+    inspect(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-  };
+  },
 });
