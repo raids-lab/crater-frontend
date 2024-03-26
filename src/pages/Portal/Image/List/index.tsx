efbd5590fc6d1d887fc465f82e4551db74f55d02
huangsy <1845930603@qq.com>
2024-03-26 09:49:12 +0000

    feat(component): feature: 镜像打包页面v1.0

---
+++ src/pages/Portal/Image/List/index.tsx
@@ -0,0 +1,242 @@
+import { DataTableToolbarConfig } from "@/components/custom/DataTable/DataTableToolbar";
+import { useQuery } from "@tanstack/react-query";
+import { useMemo, type FC, useState } from "react";
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTableColumnHeader } from "@/components/custom/DataTable/DataTableColumnHeader";
+import { DataTable } from "@/components/custom/OldDataTable";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+// import { statuses } from "../../Job/Ai/statuses";
+import { cn } from "@/lib/utils";
+import {
+  CheckCircledIcon,
+  CircleIcon,
+  ClockIcon,
+  CrossCircledIcon,
+  StopwatchIcon,
+} from "@radix-ui/react-icons";
+import instance, { VERSION } from "@/services/axios";
+import { IResponse } from "@/services/types";
+import { NewTaskForm } from "./Form";
+import { TableDate } from "@/components/custom/TableDate";
+
+type ImagePackInfo = {
+  id: number;
+  nametag: string;
+  link: string;
+  status: string;
+  createdAt: string;
+};
+
+type ImagePackStatusValue =
+  | "Initial"
+  | "Pending"
+  | "Running"
+  | "Finished"
+  | "Failed";
+
+const imagepack_statuses: {
+  value: ImagePackStatusValue;
+  label: string;
+  icon: React.ComponentType<{ className?: string }>;
+}[] = [
+  {
+    value: "Initial",
+    label: "检查中",
+    icon: CircleIcon,
+  },
+  {
+    value: "Pending",
+    label: "等待中",
+    icon: ClockIcon,
+  },
+  {
+    value: "Running",
+    label: "运行中",
+    icon: StopwatchIcon,
+  },
+  {
+    value: "Finished",
+    label: "成功",
+    icon: CheckCircledIcon,
+  },
+  {
+    value: "Failed",
+    label: "失败",
+    icon: CrossCircledIcon,
+  },
+];
+
+const getHeader = (key: string): string => {
+  switch (key) {
+    case "nametag":
+      return "名称";
+    case "link":
+      return "链接";
+    case "status":
+      return "状态";
+    case "createdAt":
+      return "创建时间";
+    default:
+      return key;
+  }
+};
+
+const toolbarConfig: DataTableToolbarConfig = {
+  filterInput: {
+    placeholder: "搜索镜像",
+    key: "link",
+  },
+  filterOptions: [],
+  getHeader: getHeader,
+};
+
+const columns: ColumnDef<ImagePackInfo>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="ml-2"
+      />
+    ),
+    cell: ({ row }) => (
+      <>
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+          className="ml-2"
+        />
+      </>
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "nametag",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title={getHeader("nametag")} />
+    ),
+    cell: ({ row }) => <div>{row.getValue("nametag")}</div>,
+  },
+  {
+    accessorKey: "link",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title={getHeader("link")} />
+    ),
+    cell: ({ row }) => <div>{row.getValue("link")}</div>,
+  },
+  {
+    accessorKey: "status",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title={getHeader("status")} />
+    ),
+    cell: ({ row }) => {
+      const status = imagepack_statuses.find(
+        (status) => status.value === row.getValue("status"),
+      );
+      if (!status) {
+        return null;
+      }
+      return (
+        <div className="flex flex-row items-center justify-start">
+          <div
+            className={cn("flex h-3 w-3 rounded-full", {
+              "bg-purple-500 hover:bg-purple-400": status.value === "Initial",
+              "bg-slate-500 hover:bg-slate-400": status.value === "Pending",
+              "bg-sky-500 hover:bg-sky-400": status.value === "Running",
+              "bg-red-500 hover:bg-red-400": status.value === "Failed",
+              "bg-emerald-500 hover:bg-emerald-400":
+                status.value === "Finished",
+            })}
+          ></div>
+          <div className="ml-1.5">{status.label}</div>
+        </div>
+      );
+    },
+    filterFn: (row, id, value) => {
+      return (value as string[]).includes(row.getValue(id));
+    },
+  },
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title={getHeader("createdAt")} />
+    ),
+    cell: ({ row }) => {
+      return <TableDate date={row.getValue("createdAt")}></TableDate>;
+    },
+    sortingFn: "datetime",
+  },
+];
+
+type ImagePackInfoResponse = {
+  ID: number;
+  // name: string;
+  imagelink: string;
+  status: string;
+  createdAt: string;
+  nametag: string;
+};
+
+export const ImagePackList = () =>
+  instance.get<IResponse<ImagePackInfoResponse[]>>(VERSION + "/image/list");
+
+export const Component: FC = () => {
+  const [openSheet, setOpenSheet] = useState(false);
+  const imagePackInfo = useQuery({
+    queryKey: ["imagelink", "status"],
+    queryFn: () => ImagePackList(),
+    select: (res) => res.data.data,
+  });
+  const data: ImagePackInfo[] = useMemo(() => {
+    if (!imagePackInfo.data) {
+      return [];
+    }
+    return imagePackInfo.data.map((item, index) => ({
+      id: index,
+      link: item.imagelink,
+      status: item.status,
+      createdAt: item.createdAt,
+      nametag: item.nametag,
+    }));
+  }, [imagePackInfo.data]);
+
+  return (
+    <div className="space-y-1 text-xl">
+      <DataTable
+        data={data}
+        columns={columns}
+        toolbarConfig={toolbarConfig}
+        loading={false}
+      >
+        <Sheet open={openSheet} onOpenChange={setOpenSheet}>
+          <SheetTrigger asChild>
+            <Button className="h-8 min-w-fit">创建镜像</Button>
+          </SheetTrigger>
+          {/* scroll in sheet: https://github.com/shadcn-ui/ui/issues/16 */}
+          <SheetContent className="max-h-screen overflow-y-auto sm:max-w-3xl">
+            <SheetHeader>
+              <SheetTitle>创建镜像</SheetTitle>
+              <SheetDescription>创建一个新的训练任务镜像</SheetDescription>
+            </SheetHeader>
+            <Separator className="mt-4" />
+            <NewTaskForm closeSheet={() => setOpenSheet(false)} />
+          </SheetContent>
+        </Sheet>
+      </DataTable>
+    </div>
+  );
+};
