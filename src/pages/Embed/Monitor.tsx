54b78f3fca0f60f128637695a0f376357170db5e
Yilong Li <mondaycha@outlook.com>
2025-01-03 23:31:22 +0800

    fix: grafana theme

--- src/pages/Embed/Monitor.tsx
+++ src/pages/Embed/Monitor.tsx
@@ -1,14 +1,26 @@
 import { useTheme } from "@/utils/theme";
-import { type FC } from "react";
+import { useMemo, type FC } from "react";
 
 const GPU_DASHBOARD = import.meta.env.VITE_GRAFANA_GPU_DASHBOARD;
 
 export const GrafanaIframe = ({ baseSrc }: { baseSrc: string }) => {
   const { theme } = useTheme();
+
+  // if baseURL does not have parameter, add ?, else if baseURL does not ends with &, add &
+  const base = useMemo(() => {
+    if (baseSrc.indexOf("?") === -1) {
+      return `${baseSrc}?`;
+    } else if (baseSrc.endsWith("&") === false) {
+      return `${baseSrc}&`;
+    } else {
+      return baseSrc;
+    }
+  }, [baseSrc]);
+
   return (
     <iframe
       title="grafana"
-      src={`${baseSrc}?theme=${theme}&kiosk`}
+      src={`${base}theme=${theme}&kiosk`}
       height={"100%"}
       width={"100%"}
     />
