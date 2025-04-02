13ca0f13bcdee3baa0ce0be837a32aecec289eb8
LeiXZ <2459605339@qq.com>
2025-04-02 17:38:00 +0800

    fix: fix createdataset

--- src/components/custom/ModelTable.tsx
+++ src/components/custom/ModelTable.tsx
@@ -1,32 +1,27 @@
-import { Button } from "@/components/ui/button";
-import DataList from "@/pages/Portal/Data/DataList";
-import DocsButton from "@/components/button/DocsButton"; //, CirclePlusIcon
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
 import { Dataset } from "@/services/api/dataset";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { Separator } from "@radix-ui/react-dropdown-menu";
+import DocsButton from "@/components/button/DocsButton";
 import { DatasetCreateForm } from "@/pages/Portal/Data/CreateForm";
-
 import { IResponse } from "@/services/types";
+import DataList from "@/pages/Portal/Data/DataList";
 import { AxiosResponse } from "axios";
+import SandwichSheet from "@/components/sheet/SandwichSheet";
+
 interface DatesetTableProps {
   apiGetDataset: () => Promise<AxiosResponse<IResponse<Dataset[]>>>;
 }
+
 export function ModelTable({ apiGetDataset }: DatesetTableProps) {
   const data = useQuery({
     queryKey: ["data", "mydataset"],
     queryFn: () => apiGetDataset(),
     select: (res) => res.data.data,
   });
+
   const [openSheet, setOpenSheet] = useState(false);
+
   return (
     <DataList
       items={
@@ -36,9 +31,9 @@ export function ModelTable({ apiGetDataset }: DatesetTableProps) {
             id: dataset.id,
             name: dataset.name,
             desc: dataset.describe,
-            tag: dataset.extra.tag || [], // Add appropriate tags if available
+            tag: dataset.extra.tag || [],
             createdAt: dataset.createdAt,
-            owner: dataset.attribute, // Adjust to match IUserAttributes structure
+            owner: dataset.attribute,
             username: dataset.username,
           })) || []
       }
@@ -46,33 +41,23 @@ export function ModelTable({ apiGetDataset }: DatesetTableProps) {
       actionArea={
         <div className="flex flex-row gap-3">
           <DocsButton title="模型文档" url="file/model" />
-          {/*<Button>
-            <CirclePlusIcon />
-            导入模型
-          </Button>*/}
-          <Sheet open={openSheet} onOpenChange={setOpenSheet}>
-            <SheetTrigger asChild>
-              <Button className="h-8 min-w-fit">创建模型</Button>
-            </SheetTrigger>
-            <SheetContent className="max-h-screen overflow-y-auto sm:max-w-3xl">
-              <SheetHeader>
-                <SheetTitle>创建新模型</SheetTitle>
-                <SheetDescription>创建一个新的文件模型集</SheetDescription>
-              </SheetHeader>
-              <Separator className="mt-4" />
+          <SandwichSheet
+            isOpen={openSheet}
+            onOpenChange={setOpenSheet}
+            title="创建模型"
+            description="创建一个新的文件模型集"
+            trigger={<Button className="h-8 min-w-fit">创建模型</Button>}
+            className="sm:max-w-3xl"
+          >
+            <div className="pt-1">
               <DatasetCreateForm
                 closeSheet={() => setOpenSheet(false)}
                 type="model"
               />
-            </SheetContent>
-          </Sheet>
+            </div>
+          </SandwichSheet>
         </div>
       }
     />
   );
 }
-/*const Model = () => {
-  
-};
-
-export default Model;*/
