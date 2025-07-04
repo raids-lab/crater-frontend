fc91e71d0511db1a9c3b38d5066f8918487f6a61
Yilong Li <mondaycha@outlook.com>
2025-07-05 00:00:36 +0800

    fix: homepage ui

--- src/components/chart/PieCard.tsx
+++ src/components/chart/PieCard.tsx
@@ -44,9 +44,7 @@ const PieCard = ({
           </TooltipProvider>
         </CardTitle>
       </CardHeader>
-      <div className="flex h-52 items-center justify-center px-2">
-        {children}
-      </div>
+      <div className="relative h-52">{children}</div>
     </Card>
   );
 };
